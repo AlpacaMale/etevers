0c2294631f8618912311f28d519365df60f24f33
박병준 <77188449+AlpacaMale@users.noreply.github.com>
2024-06-10 18:29:11 +0900

    webhook test

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# webhook test
 from flask import Flask, render_template, request, session, redirect, jsonify
 from function import login_required, get_db, teardown_request, error
 from datetime import date as dt_date
