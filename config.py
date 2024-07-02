64aa43b364b429899b18718b54e7ede17e1859ef
YunaJ-02 <156779056+YunaJ-02@users.noreply.github.com>
2024-07-02 09:16:20 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://user:root@localhost/mydb"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@172.16.0.201:3306/mydb"
     # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:root1234@rds.cluster-crqgcai442on.ap-northeast-2.rds.amazonaws.com:3306/mydb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "your_secret_key"
