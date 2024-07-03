ae57c3ae26e860df3a2e119ef5435a42d38d65e0
박병준 <77188449+AlpacaMale@users.noreply.github.com>
2024-07-03 12:41:08 +0900

    db address

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
     SQLALCHEMY_BINDS = {
-        "db": "mysql+pymysql://user:root@localhost:3306/mydb",
+        "db": "mysql+pymysql://root:12345678@172.16.0.201:3306/mydb",
         "rds": "mysql+pymysql://user:root@localhost:3306/mydb",
     }
     # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:12345678@172.16.0.201:3306/mydb"
